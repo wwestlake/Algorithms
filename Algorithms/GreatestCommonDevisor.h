d695902d5d1ad68b9375554b98700b24ef6b0f1a
Bill Westlake <wwestlake@lagdaemon.com>
2020-01-19 21:16:42 -0500

    Added Comments to GCD

--- Algorithms/GreatestCommonDevisor.h
+++ Algorithms/GreatestCommonDevisor.h
@@ -11,13 +11,17 @@ private:
 
 public:
 
+	/*
+		Algorithm calculates Greatest Common Devisor according to Euclids Algorithm
+	*/
 	static int Calculate(int m, int n)
 	{
 		int register m1 = 0;
 		int register n1 = 0;
 		int register r = 0;
 
-		if (m > n) {
+		// If n > m we want to swap the values
+		if (m >= n) {
 			m1 = m;
 			n1 = n; 
 		}
@@ -27,14 +31,17 @@ public:
 		}
 
 		do {
+			// find the remainder of m / n for this iteration
 			r = m1 % n1;
 
+			// if the remainder is non-zero we shift n to m and r to n then recalculate
 			if (r > 0)
 			{
 				m1 = n1;
 				n1 = r;
 			}
 			else {
+				// n1 contains the greates common devisor of m and n
 				return n1;
 			}
 
