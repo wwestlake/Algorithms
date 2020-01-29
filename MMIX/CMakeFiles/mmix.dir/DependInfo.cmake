ae6df92b4105a38e5764a5e3eec28ca2c6abc9a0
William Westlake <wwestlake@lagdaemon.com>
2020-01-28 21:34:04 -0500

    added boost to cmake

--- MMIX/CMakeFiles/mmix.dir/DependInfo.cmake
+++ MMIX/CMakeFiles/mmix.dir/DependInfo.cmake
@@ -8,8 +8,15 @@ set(CMAKE_DEPENDS_CHECK_CXX
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_DYN_LINK"
+  )
+
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "MMIX/include"
+  "/home/wwestlake/boost/include"
   )
 
 # Targets to which this target links.
